91aab14cd06cdf9de8eece4ee1b4d9c35f407d64
schekatz <bernhard.schekatz@livingsolids.de>
2022-02-04 16:28:34 +0100

    Created new project.
    copied Project Stetting and Packages
    added Nespar Project withcout compiled binaries (Without bin and obj )

---
+++ NesperUnityIntegration/Assets/Integrationstests/Nesper611Instancing.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.AR_VR_WrapperFramework;
+
+public class Nesper611Instancing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var test = new Nesper_611_test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
